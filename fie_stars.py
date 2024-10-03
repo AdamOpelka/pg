98331718d43243add1813e5d09cacf5fbf4dff49
AdamOpelka <opelka02@jcu.cz>
2024-10-03 09:16:05 +0000

    zmeny

---
+++ fie_stars.py
@@ -0,0 +1,41 @@
+# funkce demonstující while
+"""
+def starts_while():
+    print("zacatek")
+
+    i = 0
+
+    while i<5:
+        print("*")
+        i+=1
+
+    print("konec")
+
+# funkce demonstujici for
+
+def starts_for():
+    print("zacatek")
+
+    for i in [0, 1, 2, 3, 4]:
+        print("*",i)
+
+        print("konec"
+"""
+#funkce demonstruje min, max, if
+
+def in_range(min_number, max_number, number):
+    if (number < max_number and number > min_number):
+        print("Is in range")
+    else:
+        print("Is not in range")
+#in_range(100, 1000, 110)
+
+
+#funkce vrací string "Ahoj jmeno"
+def dopln_jmeno(jmeno):
+    print("Jmenujete se", jm)
+
+jm = input("Zadej jméno:")
+dopln_jmeno(jm)
+
+
--- first.py
+++ first.py
@@ -1,4 +1,14 @@
+""""
 def hello():
     print("Hello World!")
+"""
+def sudy_lichy(cislo):
+    if (cislo%2 == 0):
+        print("Číslo je sudé")
+    elif (cislo%2 != 0):
+        print("Číslo je liché")
+    else:
+        print("Nezadal jsi číslo")
 
-hello()
+cislo = input("Zadej číslo:")
+sudy_lichy(cislo)
