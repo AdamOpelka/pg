1b914200af18006c4a055bf26a9c36fcd94f1c20
AdamOpelka <opelka02@jcu.cz>
2025-01-10 08:15:09 +0000

    hotovo

--- zkouska_cv2.py
+++ zkouska_cv2.py
@@ -9,6 +9,7 @@
 # Pokud zadaná měna v kurzovním lístku neexistuje, vyhoďte výjimku `ValueError`.
 
 import requests
+from unittest.mock import patch, MagicMock
 
 def convert_to_czk(amount, currency):
     # URL kurzovního lístku
@@ -37,8 +38,24 @@ def convert_to_czk(amount, currency):
             return round(amount * rate / amount_in_currency, 2)
 
     # Pokud měna nebyla nalezena, vyhoďte výjimku
-    raise ValueError(f"Měna {currency} nebyla nalezena v kurzovním lístku.")
+    raise ValueError(f"Currency {currency} not found in the exchange rate list.")
 
-print(convert_to_czk(100, "USD"))
-print(convert_to_czk(50, "EUR"))
-#print(convert_to_czk(100, "ZEN"))
+def test_convert_to_czk():
+    mock_response = """31.10.2025 #237
+země|měna|množství|kód|kurz
+Austrálie|dolar|1|AUD|14,894
+EMU|euro|1|EUR|25,480
+USA|dolar|1|USD|23,000
+Velká Británie|libra|1|GBP|29,745
+"""
+    with patch("requests.get") as mock_get:
+        mock_get.return_value = MagicMock(ok=True, status_code=200, text=mock_response)
+
+        assert convert_to_czk(100, "USD") == 2300.00
+        assert convert_to_czk(50, "EUR") == 1274.00
+        assert convert_to_czk(200, "AUD") == 2978.80
+        
+        try:
+            convert_to_czk(100, "XYZ")
+        except ValueError as e:
+            assert str(e) == "Currency XYZ not found in the exchange rate list."
